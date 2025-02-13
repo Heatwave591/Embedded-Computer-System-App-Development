13864b423b14c2f22083b2be374fb1f7686325d3
Anirudhhan Raghuraman <anirudh.raghu591@gmail.com>
2025-02-13 06:20:47 -0500

    fixed Lab 2

--- lab2/IR/IR.ino
+++ lab2/IR/IR.ino
@@ -22,19 +22,22 @@
 
 // }
 
-const int sensorPin = 23;
-const int led1 = 22;
-const int led2 = 21;
-const int led3 = 19;
-const int led4 = 18;
+const int sensorPin = 9;
+const int led1 = 3;
+// const int led2 = 3;
+// const int led3 = 4;
+// const int led4 = 5;
+// const int led5 = 6;
+// const int led6 = 7;
+
 void setup() {
   Serial.begin(115200);
 
   pinMode(sensorPin, INPUT);
   pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
+  // pinMode(led2, OUTPUT);
+  // pinMode(led3, OUTPUT);
+  // pinMode(led4, OUTPUT);
 
 
 }
@@ -45,18 +48,22 @@ void loop() {
   if (sensorValue == LOW) {
     Serial.println("Object detected");
     digitalWrite(led1, HIGH);
-    digitalWrite(led2, HIGH);
-    digitalWrite(led3, HIGH);
-    digitalWrite(led4, HIGH);
+    // digitalWrite(led2, HIGH);
+    // digitalWrite(led3, HIGH);
+    // digitalWrite(led4, HIGH);
+    // digitalWrite(led5, HIGH);
+    // digitalWrite(led6, HIGH);
 
   } 
   
   else {
   Serial.println("No object detected");
     digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
-    digitalWrite(led4, LOW);
+    // digitalWrite(led2, LOW);
+    // digitalWrite(led3, LOW);
+    // digitalWrite(led4, LOW);
+    // digitalWrite(led5, LOW);
+    // digitalWrite(led6, LOW);
 }
 
 delay(1000);
